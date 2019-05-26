efab2a7ef75a3335e557377ef8688e83df388d4b
Marcos Moreno <marcosmorenomamg@gmail.com>
2019-05-26 14:21:04 -0600

    dockerizing app, adding phpmyadmin and mysql

--- surveyapp/settings.py
+++ surveyapp/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'surveyapp.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'surveyapp',
+        'USER': 'django',
+        'PASSWORD': 'abc123',
+        'HOST': 'mysql',
+        'PORT': '3306',  # Default MySQL port
     }
+
 }
 
 
