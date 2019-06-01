f09a8cb089883ec2aadde43ef28022854bbc7653
Marcos Moreno <marcosmorenomamg@gmail.com>
2019-05-31 23:15:15 -0600

    Update production.py

--- surveyapp/settings/production.py
+++ surveyapp/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY'),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["marcosmoreno.dev", "138.68.59.215"]  # for testing purposes only
+ALLOWED_HOSTS = ["*"]  # for testing purposes only
 
 ADMINS = (
     (
